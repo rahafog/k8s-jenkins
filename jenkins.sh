5c0544f70122aaada1b2cee512f843797410fa3f
Bryan Dollery <bryan.dollery@gmail.com>
2020-08-26 12:12:31 +0300

    refactored cluster operations into a different repo, so this just deploys jenkins and dumps the credentials to the console

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-k3d cluster create jenkins -v /var/run/docker.sock:/var/run/docker.sock --agents 3 -p 30000-32767:30000-32767@server[0]
-source <(k3d completion bash)
-source <(kubectl completion bash)
-kubectl apply -f jenkins.namespace.yaml
-kubectl apply -f jenkins.helm.yaml
-WAIT=60
+kubectl apply -f jenkins.namespace.yaml -f jenkins.helm.yaml
+WAIT=90
 echo "Sleeping for $WAIT"
 sleep $WAIT
 echo "Making progress"
