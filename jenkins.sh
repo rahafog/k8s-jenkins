1c89054b1a30cdce9f8e75ec7d28b1c38a242888
Bryan Dollery <bryan.dollery@knowledgehut.co>
2020-08-26 10:27:18 +0000

    fix typo

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl apply -f jenkins.namespace.yaml -f jenkins.helm.yaml -f ingress.sh
+kubectl apply -f jenkins.namespace.yaml -f jenkins.helm.yaml -f ingress.yaml
 WAIT=90
 echo "Sleeping for $WAIT"
 sleep $WAIT
